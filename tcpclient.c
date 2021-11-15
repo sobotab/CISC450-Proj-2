80ef4d4f97f896774db11732391c29e0dd3c85b0
sobotab <sobotab@udel.edu>
2021-11-15 15:11:44 -0500

    added comments to create outline for client process

--- tcpclient.c
+++ tcpclient.c
@@ -1,7 +1,8 @@
 #include "tcp.h"
 
 int main(void) {
-
+   
+   //Variables defined to be used by the client backend
    int sock_client;  /* Socket used by client */
 
    struct sockaddr_in client_addr;  /* Internet address structure that
@@ -13,16 +14,13 @@ int main(void) {
                                         address */
    char server_hostname[STRING_SIZE]="localhost"; /* Server's hostname */
    unsigned short server_port=46238;  /* Port number used by server (remote port) */
+   unsigned short client_port=CLNT_TCP_PORT; /* Port number used by client (local port) */
 
-   unsigned short client_port=CLNT_TCP_PORT;
-
-   char sentence[STRING_SIZE]="localhost";  /* send message */
-   char modifiedSentence[STRING_SIZE]; /* receive message */
-   unsigned int msg_len;  /* length of message */                      
-   int bytes_sent, bytes_recd; /* number of bytes sent or received */
+   //Message information to be used in this application
+   message_t rec_message;
+   message_t ret_message;
   
    /* open a socket */
-
    if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
       perror("Client: can't open stream socket");
       exit(1);
@@ -37,7 +35,6 @@ int main(void) {
    client_addr.sin_port = htons(client_port);
    
    /* bind the socket to the local client port */
-
    if (bind(sock_client, (struct sockaddr *) &client_addr,
                                     sizeof (client_addr)) < 0) {
       perror("Client: can't bind to local address\n");
@@ -46,7 +43,6 @@ int main(void) {
    }
 
    /* initialize server address information */
-
    if ((server_hp = gethostbyname(server_hostname)) == NULL) {
       perror("Client: invalid server hostname");
       close(sock_client);
@@ -61,28 +57,28 @@ int main(void) {
    server_addr.sin_port = htons(server_port);
 
     /* connect to the server */
- 		
    if (connect(sock_client, (struct sockaddr *) &server_addr, 
                                     sizeof (server_addr)) < 0) {
       perror("Client: can't connect to server");
       close(sock_client);
       exit(1);
    }
+
+   /*Create message*/
+
+   /*Convert message from host to network*/
+
+   /*Print message
   
-   /* user interface */
-
-   printf("Please input a sentence:\n");
-   scanf("%s", sentence);
-   msg_len = strlen(sentence) + 1;
-
    /* send message */
-   
    bytes_sent = send(sock_client, sentence, msg_len, 0);
 
    /* get response from server */
-  
    bytes_recd = recv(sock_client, modifiedSentence, STRING_SIZE, 0); 
 
+   /*Convert message from network to host*/
+
+   /*Print received message*/
    printf("\nThe response from server is:\n");
    printf("%s\n\n", modifiedSentence);
 
