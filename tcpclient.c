01dfaa230b3a36a922a8c2a3898e1c457175bc91
rstahl2000 <rstahl@udel.edu>
2021-12-01 22:16:28 -0500

    Adds comments and prints
    Adds comments for graders to better understand code. Comments out debug prints to make output better. Also adds new prints to show the received and sent message.

--- tcpclient.c
+++ tcpclient.c
@@ -16,9 +16,9 @@ int main(void) {
    int bytes_recd;
    unsigned short server_port=46238;  /* iPort number used by server (remote port) */
    unsigned short client_port=CLNT_TCP_PORT; /* Port number used by client (localort)*/
-   unsigned short int step_no=1;
-   unsigned short int server_port_no=48000;
-   unsigned short int server_secret_no=0;
+   unsigned short int step_no=1; // Variable to hold step of current message
+   unsigned short int server_port_no=48000; //Variable for current port number
+   unsigned short int server_secret_no=0; // Variable to hold current server secret number
 
    struct timeval timeout;
    timeout.tv_sec=1;
@@ -27,13 +27,16 @@ int main(void) {
    message_t *rec_message=(message_t *)malloc(sizeof(message_t));
    message_t *ret_message=(message_t *)malloc(sizeof(message_t));
   
+   // Flag to store whether current server is on step 3
    int done=0;
 
-   /* open a socket */
+   //For each server port number in range
    for(; server_port_no<48999; server_port_no++){
+	// Open Travel file for getting current server info
 	FILE* travel_log1 = fopen("Travel.txt", "r");
         char line1[STRING_SIZE];
-
+		
+		// Traverse each line in the file, check the step number and store data
         while (fgets(line1, sizeof(line1), travel_log1)) {
               step_no=atoi(strtok(line1, ","));
               if(server_port_no == atoi(strtok(NULL, ",")) && step_no <= 3) {
@@ -48,17 +51,19 @@ int main(void) {
 			server_secret_no=0;
 		}
         }
+	// If current server is on step 3, continue to next number
 	if(done) {
 		done=0;
 		continue;
 	}
 	fclose(travel_log1);
 
+
    	if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
       		perror("Client: can't open stream socket");
       		exit(1);
    	}
-
+	// Open socket
 	setsockopt(sock_client, SOL_SOCKET, SO_RCVTIMEO, (const void*) &timeout,
 			sizeof(timeout));
 
@@ -120,6 +125,10 @@ int main(void) {
 		ret_message->server_secret_no=server_secret_no;
 		strcpy(ret_message->text, "sobota-stahl");
 	}
+	//Print Message to be sent
+	printf("Message Sent: %d, %d, %d, %s\n",ret_message->step_no,ret_message->server_port_no,
+	ret_message->server_secret_no,ret_message->text);
+
 	ret_message->step_no=htons(ret_message->step_no);
 	ret_message->server_port_no=htons(ret_message->server_port_no);
 	ret_message->server_secret_no=htons(ret_message->server_secret_no);
@@ -139,12 +148,17 @@ int main(void) {
 	for (int i=0; rec_message->text[i] || i<80; i++) {
 		rec_message->text[i]=tolower(rec_message->text[i]);
 	}
+	//Print message received
+	printf("Message Rcvd: %d, %d, %d, %s\n",rec_message->step_no,rec_message->server_port_no,
+	rec_message->server_secret_no,rec_message->text);
+	/*  Printf's used for debugging
 	printf("Rec step_no: %hu\n", rec_message->step_no);
 	printf("Rec client_port_no: %hu\n", rec_message->client_port_no);
 	printf("Rec server_port_no: %hu\n", rec_message->server_port_no);
 	printf("Rec server_secret_no: %hu\n", rec_message->server_secret_no);
-	printf("Rec text: %s\n", rec_message->text);
+	printf("Rec text: %s\n", rec_message->text);*/
 
+	//Initializes variables for storing message data in txt files
 	FILE* travel_log = fopen("Travel.txt", "r");
       	FILE* tmp_travel_log = fopen("tempTravel.txt", "a");
       	char line[STRING_SIZE];
@@ -159,7 +173,7 @@ int main(void) {
               strtok(new_line, ",");
               tmp_server_port=atoi(strtok(NULL, ","));
               if (tmp_server_port != rec_message->server_port_no) {
-                      printf("server that already exists: %s\n", line);
+                      //printf("server that already exists: %s\n", line);
 		      fprintf(tmp_travel_log, "%s", line);
               } else {
 		      strcpy(error_line, line);
@@ -169,7 +183,7 @@ int main(void) {
 	char tmp_str[STRING_SIZE];
 	if (step_no == rec_message->step_no) {
              	memset(tmp_str, '\0', STRING_SIZE);
-		printf("step no: %d\n", rec_message->step_no);
+		//printf("step no: %d\n", rec_message->step_no);
              	sprintf(tmp_str, "%d", rec_message->step_no);
              	strcat(new_line, tmp_str);
              	strcat(new_line, ",");
@@ -177,14 +191,14 @@ int main(void) {
              	sprintf(tmp_str, "%d", rec_message->server_port_no);
              	strcat(new_line, tmp_str);
 		strcat(new_line, ",");
-		printf("new_line half way: %s\n", new_line);
+		//printf("new_line half way: %s\n", new_line);
 		memset(tmp_str, '\0', STRING_SIZE);
 		sprintf(tmp_str, "%d", rec_message->server_secret_no);
 		strcat(new_line, tmp_str);
              	strcat(new_line, ",");
              	strcat(new_line, rec_message->text);
              	strcat(new_line, "\n");
-		printf("new_line: %s\n", new_line);
+		//printf("new_line: %s\n", new_line);
 		fprintf(tmp_travel_log, "%s", new_line);
 	} else {
 		fprintf(tmp_travel_log, "%s", error_line);
